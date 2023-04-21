345efbbf96dec5b2359df33faac118873e8fd2e5
Yan Doroshenko <yan1994doroshenko@gmail.com>
2023-04-21 15:02:36 +0200

    [wip] WIP: Cats effect tutorial

--- src/main/scala/com/github/yandoroshenko/cats/tutorial/FileCopier.scala
+++ src/main/scala/com/github/yandoroshenko/cats/tutorial/FileCopier.scala
@@ -1,42 +1,42 @@
 package com.github.yandoroshenko.cats.tutorial
 
-import cats.effect.{IO, Resource}
+import cats.effect.{Sync, Resource}
 import cats.syntax.all._
 import java.io._
 
 object FileCopier {
 
-  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
+  def transfer[F[_] : Sync](origin: InputStream, destination: OutputStream): F[Long] =
     transmit(origin, destination, new Array[Byte](1024 * 10), 0L)
 
-  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
+  def transmit[F[_] : Sync](origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): F[Long] =
     for {
-      amount <- IO.blocking(origin.read(buffer, 0, buffer.size))
+      amount <- Sync[F].blocking(origin.read(buffer, 0, buffer.size))
       count <- if (amount > -1) {
-        IO.blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
+        Sync[F].blocking(destination.write(buffer, 0, amount)) >> transmit(origin, destination, buffer, acc + amount)
       } else {
-        IO.pure(acc)
+        Sync[F].pure(acc)
       }
     } yield count
 
-  def copy(origin: File, destination: File): IO[Long] =
+  def copy[F[_] : Sync](origin: File, destination: File): F[Long] =
     inputOutputStreams(origin, destination).use(transfer)
 
-  def inputStream(f: File): Resource[IO, FileInputStream] =
+  def inputStream[F[_] : Sync](f: File): Resource[F, FileInputStream] =
     Resource.make {
-      IO.blocking(new FileInputStream(f))
+      Sync[F].blocking(new FileInputStream(f))
     } { inStream =>
-      IO.blocking(inStream.close()).handleErrorWith(_ => IO.unit)
+      Sync[F].blocking(inStream.close()).handleErrorWith(_ => Sync[F].unit)
     }
 
-  def outputStream(f: File): Resource[IO, FileOutputStream] =
+  def outputStream[F[_] : Sync](f: File): Resource[F, FileOutputStream] =
     Resource.make {
-      IO.blocking(new FileOutputStream(f))
+      Sync[F].blocking(new FileOutputStream(f))
     } { outStream =>
-      IO.blocking(outStream.close()).handleErrorWith(_ => IO.unit)
+      Sync[F].blocking(outStream.close()).handleErrorWith(_ => Sync[F].unit)
     }
 
-  def inputOutputStreams(in: File, out: File): Resource[IO, (FileInputStream, FileOutputStream)] =
+  def inputOutputStreams[F[_] : Sync](in: File, out: File): Resource[F, (FileInputStream, FileOutputStream)] =
     for {
       i <- inputStream(in)
       o <- outputStream(out)
--- src/main/scala/com/github/yandoroshenko/cats/tutorial/MainApp.scala
+++ src/main/scala/com/github/yandoroshenko/cats/tutorial/MainApp.scala
@@ -10,7 +10,7 @@ object MainApp extends IOApp {
       _ <- if (args.length < 2) IO.raiseError(new IllegalArgumentException("Specify origin and destination file names")) else IO.unit
       origin = new File(args(0))
       destination = new File (args(1))
-      count <- FileCopier.copy(origin, destination)
+      count <- FileCopier.copy[IO](origin, destination)
       _ <- IO.println(s"Copied $count bytes from ${origin.getPath} to ${destination.getPath}")
     } yield ExitCode.Success
 }
